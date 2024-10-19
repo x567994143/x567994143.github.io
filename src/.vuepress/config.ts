3a3d6efc37e3952b978f911adaab6464c1f11a60
x567994143 <2941377716@qq.com>
2024-10-19 22:36:47 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/my-blog/",
+  base: "/",
 
   lang: "zh-CN",
   title: "博客演示",
